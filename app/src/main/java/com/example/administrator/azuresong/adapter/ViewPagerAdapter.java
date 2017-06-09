c7b66304b30ea7b32eb0d1ccbbafe01a12ad40bb
Azuresong <fangzhx_sdcors@163.com>
2017-06-10 07:50:10 +0800

    ViewPager/Fragment
    TODO:viewPager.setAdapter

---
+++ app/src/main/java/com/example/administrator/azuresong/adapter/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.administrator.azuresong.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/6/10 0010.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+}
--- app/src/main/java/com/example/administrator/azuresong/module/ZhihuDaily/ZhihuActivity.java
+++ app/src/main/java/com/example/administrator/azuresong/module/ZhihuDaily/ZhihuActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.administrator.azuresong.R;
+import com.example.administrator.azuresong.adapter.ViewPagerAdapter;
 import com.example.administrator.azuresong.module.Base.BaseActivity;
+import com.example.administrator.azuresong.module.Base.BaseFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,9 +22,12 @@ import butterknife.ButterKnife;
 
 public class ZhihuActivity extends BaseActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-
+    @BindView(R.id.viewpager)
+    ViewPager mViewpager;
     @BindView(R.id.bnv_menu)
     BottomNavigationView mBnvMenu;
+
+    private MenuItem mMenuItem;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,15 +36,55 @@ public class ZhihuActivity extends BaseActivity implements BottomNavigationView.
         ButterKnife.bind(this);
 
         mBnvMenu.setOnNavigationItemSelectedListener(this);
+
+        mViewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (mMenuItem != null) {
+                    mMenuItem.setChecked(false);
+                } else {
+                    mBnvMenu.getMenu().getItem(0).setChecked(false);
+                }
+                mMenuItem = mBnvMenu.getMenu().getItem(position);
+                mMenuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(mViewpager);
+    }
+
+    private void setupViewPager(ViewPager viewpager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(BaseFragment.newInstance("新闻"));
+        adapter.addFragment(BaseFragment.newInstance("图书"));
+        adapter.addFragment(BaseFragment.newInstance("发现"));
+        viewPager.setAdapter(adapter);
     }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Log.d("TAG", item.getItemId() + " item was selected-------------------");
-        return true;
-
+        switch (item.getItemId()) {
+            case R.id.action_zhihu:
+                mViewpager.setCurrentItem(0);
+                break;
+            case R.id.action_meizi:
+                mViewpager.setCurrentItem(1);
+                break;
+            case R.id.action_gank:
+                mViewpager.setCurrentItem(2);
+                break;
+        }
+        return false;
     }
-
-
 }
